118e5ebebc5bbead1c338821894f287f34c615f6
Alberto Hanna <alberto.adell17@gmail.com>
2022-03-18 00:11:52 +0200

    starting calculator

--- Chapter-6_simple_calculator/GUI_6.py
+++ Chapter-6_simple_calculator/GUI_6.py
@@ -1,14 +1,38 @@
-'''lesson: '''
-class clesson(tkinter.Tk):
+import tkinter
+
+class Calclator(tkinter.Tk):
+    # colors
+    dark_green = '#93af22'
+    light_green = '#acc253'
+    white_green = '#edefe0'
+    
+    # fonts
+    button_font = ('Arial', 18)
+    display_font = ('Arial', 30)
+    
     def __init__(self):
         super().__init__()
-        self.title('Entry Basics!')
-        self.iconbitmap('thinking.ico')
-        self.geometry('500x500')
+        self.title('Calculator')
+        self.iconbitmap('calc.ico')
+        self.geometry('300x400')
         self.resizable(0, 0)
     # end def __init__
 
+    def make_layout(self):
+        # frames
+        display_frame = tkinter.LabelFrame(self)
+        button_frame = tkinter.LabelFrame(self)
+        
+        display_frame.pack()
+        button_frame.pack()
+        
+        # layout widgets
+        display = tkinter.Entry(display_frame, font=Calclator.display_font, justify=tkinter.RIGHT, bg=Calclator.white_green, width=50, borderwidth=5)
+        display.pack(padx=5, pady=5)
+        
+
     def run(self):
-        # self.create_button()
+        self.make_layout()
         self.mainloop()
-# lesson().run()
+
+Calclator().run()
