6b65bbace83fd33432c04e5f75992f68d7c65f3e
Alberto Hanna <alberto.adell17@gmail.com>
2022-03-19 01:30:28 +0200

    submit_num

--- Chapter-6_simple_calculator/GUI_6.py
+++ Chapter-6_simple_calculator/GUI_6.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import RIGHT
+from tkinter import RIGHT, END, DISABLED # right, end, disabled
 class Calculator(tkinter.Tk):
     
     def __init__(self):
@@ -29,7 +29,7 @@ class Calculator(tkinter.Tk):
         
         # layout widgets
         display = tkinter.Entry(display_frame,
-            font=display_font, justify=RIGHT,
+            font=display_font, justify='right',
             bg=white_green, width=50, borderwidth=5,
         )
         display.pack(padx=5, pady=5)
@@ -41,8 +41,8 @@ class Calculator(tkinter.Tk):
             )
             return button
         
-        clear_button = r_b('Clear', dark_green)
-        quit_button = r_b('Quit', dark_green)     
+        clear_button = r_b('Clear', dark_green, command=lambda: [display.delete(0, tkinter.END), decimal_button.config(state='normal')])
+        quit_button = r_b('Quit', dark_green, command=self.destroy)     
         
         inverse_button = r_b('1/x')
         square_button = r_b('x^2')
@@ -52,10 +52,10 @@ class Calculator(tkinter.Tk):
         substract_button = r_b('-')
         add_button = r_b('+')
         equal_button = r_b('=', dark_green)
-        decimal_button = r_b('.', 'black', fg='white')
+        decimal_button = r_b('.', 'black', fg='white', command=lambda:self.submit_number('.'))
         negate_button = r_b('+/-', 'black', fg='white')
         
-        num_buttons = [r_b(str(i),'black', fg='white') for i in range(10)] 
+        num_buttons = [r_b(str(i),'black', fg='white', command=lambda:self.submit_number(i)) for i in range(10)] 
         
         # 1st row
         clear_button.grid(row=0, column=0, columnspan=2, pady=1, sticky='WE')
@@ -66,38 +66,28 @@ class Calculator(tkinter.Tk):
                 item.grid(row=row, column=column, pady=1, sticky='WE', **kwards)
         
         # 2nd row
-        grid_em(
-            items=[inverse_button, square_button, exponent_button, divide_button],
-            row=1
-        )
-        
+        grid_em(items=[inverse_button, square_button, exponent_button, divide_button], row=1)
         # 3rd row, add padding to create the size of the columns "ipadx"
-        grid_em(
-            items=[*num_buttons[7:], multiply_button],
-            row=2,
-            ipadx=20
-        )
-        
+        grid_em(items=[*num_buttons[7:], multiply_button], row=2, ipadx=20)
         # 4th row
-        grid_em(
-            items=[*num_buttons[4:7], substract_button],
-            row=3,
-            ipadx=20
-        )
-        
+        grid_em(items=[*num_buttons[4:7], substract_button], row=3)
         # 5th row
-        grid_em(
-            items=[*num_buttons[1:4], add_button],
-            row=4
-        )
-        
-
+        grid_em(items=[*num_buttons[1:4], add_button], row=4)
         # 6th row
-        grid_em(
-            items=[negate_button, num_buttons[0], decimal_button, equal_button],
-            row=5
-        )
-
+        grid_em(items=[negate_button, num_buttons[0], decimal_button, equal_button], row=5)
+        
+        self.display = display
+        self.decimal_button = decimal_button
+    # end def make_layout
+    
+    def submit_number(self, number):
+        '''Add a numbert or decimal to the display'''
+        # insert numbert was pressed and if decimal disable the widget
+        self.display.insert('end', number)
+        
+        if '.' in self.display.get():
+            self.decimal_button.config(state='disabled')
+    
     def run(self):
         self.make_layout()
         self.mainloop()
